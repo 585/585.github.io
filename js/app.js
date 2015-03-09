f79ff8ca0228e970077beff474e1d732bde1b891
Denis Havranek <dhzabaleta@gmail.com>
2015-03-10 00:43:31 +0100

    Corrects version and example

--- js/app.js
+++ js/app.js
@@ -1,179 +1,102 @@
 (function() {
-    angular
-    .module('app', ['d'])
-    .controller('MainController', mainController);
-        function mainController() {
-            var vm = this;
-            vm.url = 'http://api.icndb.com/jokes/random/50';
-            vm.els = [];
-            vm.setup = {
-                enable: {
-                    header: true,
-                    footer: false,
-                    sorting: true,
-                    pagination: true,
-                    filters: true,
-                    checkboxes: true
-                },
-                defaults: {
-                    sort: {
-                        order: 'asc',
-                        by: 'surname'
-                    },
-                    pagination: {
-                        pageSize: 2,
-                        page: 1,
-                        pageSize: [2, 5]
-                    }
-                },
-                i18n: {
-                    locale: 'en_US',
-                    pagination: {
-                        next: 'Next',
-                        prev: 'Previous',
-                        start: 'Start',
-                        end: 'End'
-                    }
-                },
-                data: {
-                    onSuccess: function(response) {
-                        return response.value;
-                    },
-                    onError: function(response) {
-                        return response;
-                    }
-                },
-                templates: {
-                    name: '<div>This is a custom template for <b>{{$item.value}}</b></div>'
-                },
-                edit: {
-                    name: {
-                        type: 'select',
-                        data: ['Olivia', 'Alex', 'Susanne', 'Winston']
-                    },
-                    surname: {
-                        type: 'select',
-                        dataUrl: 'http://api.icndb.com/jokes/random/1'
-                    }
-                },
-                columns: {
-                    include: ['name', 'surname', 'age'],
-                    order: [],
-                    //sortables: ['surname'],
-                    labels: {
-                        name: 'My own custom name label'
-                    }
-                }
-            };
+    angular.module('app', ['d.Filters', 'd.Actions', 'd.List', 'd.Keyboard', 'd.Helpers', 'ngSanitize', 'bsDropDown'])
+        .controller('MainController', mainController);
 
-            vm.setup2 = {
-                enable: {
-                    header: true,
-                    footer: true,
-                    sorting: true,
-                    pagination: true,
-                    filters: true,
-                    checkboxes: true
-                },
-                defaults: {
-                    sort: {
-                        order: 'asc',
-                        by: 'surname'
-                    },
-                    pagination: {
-                        pageSize: 15,
-                        page: 1
-                        pageSize: [5, 10, 15, 20, 50]
-                    }
-                },
-                i18n: {
-                    locale: 'en_US',
-                    pagination: {
-                        next: 'Next',
-                        prev: 'Previous',
-                        start: 'Start',
-                        end: 'End'
-                    }
-                },
-                data: {
-                    onSuccess: function(response) {
-                        return response.value;
-                    },
-                    onError: function(response) {
-                        return response;
-                    }
-                },
-                templates: {
-                    name: '<div>This is a custom template for <b>{{$item.value}}</b></div>'
-                },
-                edit: {
-                    name: {
-                        type: 'select',
-                        data: ['Olivia', 'Alex', 'Susanne', 'Winston']
-                    },
-                    surname: {
-                        type: 'select',
-                        dataUrl: 'http://api.icndb.com/jokes/random/1'
-                    }
-                },
-                columns: {
-                    exclude: ['categories'],
-                    order: [],
-                    sortables: ['id', 'joke']
-                }
-            };
+    mainController.$inject = [];
 
-            vm.elements = [{
-                id: 1,
-                name: 'Olivia',
-                surname: 'Newton',
-                age: 32
-            }, {
-                id: 2,
-                name: 'Oliver',
-                surname: 'Night',
-                age: 12
-            }, {
-                id: 3,
-                name: 'Felicia',
-                surname: 'Low',
-                age: 43
-            }, {
-                id: 4,
-                name: 'Alex',
-                surname: 'Night',
-                age: 13
-            }, {
-                id: 5,
-                name: 'Martin',
-                surname: 'Fair',
-                age: 54
-            }];
-
-            vm.fields = [{
-                type: 'text',
-                label: 'Name',
-                ngModel: 'name',
-                size: [4,4,4,4]
+    function mainController() {
+        var vm = this;
+        vm.url = 'http://api.icndb.com/jokes/random/38';
+        vm.els = [];
+        vm.setup = {
+            enable: {
+                header: true,
+                footer: false,
+                sorting: true,
+                pagination: true,
+                filters: true,
+                checkboxes: true
             },
-            {
-                type: 'text',
-                label: 'Surname',
-                ngModel: 'surname',
-                size: [4,4,4,4]
+            defaults: {
+                sort: {
+                    order: 'asc',
+                    by: 'surname'
+                },
+                pagination: {
+                    pageSize: 2,
+                    page: 1,
+                    pageSizeOptions: [2, 5, 10]
+                }
             },
-            {
-                type: 'select',
-                label: 'Sex',
-                ngModel: 'sex',
-                ngOptions: [{
-                    key: 'male',
-                    value: 'Male'
-                }, {
-                    key: 'female',
-                    value: 'Female'
-                }],
-                size: [4,4,4,4]
-            }];
-        }
+            templates: {
+                name: '<div>This is a custom template for <b>{{$item.value}}</b></div>'
+            },
+            edit: {
+                name: {
+                    type: 'select',
+                    data: ['Olivia', 'Alex', 'Susanne', 'Winston']
+                },
+                surname: {
+                    type: 'select',
+                    dataUrl: 'http://api.icndb.com/jokes/random/1'
+                }
+            },
+            columns: {
+                include: ['name', 'surname', 'age'],
+                exclude: ['age'],
+                order: [],
+                sortables: ['surname'],
+                labels: {
+                    name: 'Name own label'
+                }
+            }
+        };
+
+        vm.elements = [{
+            id: 1,
+            name: 'Olivia',
+            surname: 'Newton',
+            age: 32
+        }, {
+            id: 2,
+            name: 'Oliver',
+            surname: 'Night',
+            age: 12
+        }, {
+            id: 3,
+            name: 'Felicia',
+            surname: 'Low',
+            age: 43
+        }, {
+            id: 4,
+            name: 'Alex',
+            surname: 'Night'
+        }, {
+            id: 5,
+            name: 'Martin',
+            surname: 'Fair',
+            age: 54
+        }];
+
+        vm.fields = [{
+            type: 'text',
+            label: 'Name',
+            ngModel: 'name'
+        }, {
+            type: 'select',
+            label: 'Sex',
+            ngModel: 'sex',
+            ngOptions: [{
+                key: 'male',
+                value: 'Male'
+            }, {
+                key: 'female',
+                value: 'Female'
+            }]
+        }, {
+            type: 'checkbox',
+            label: 'Single?',
+            ngModel: 'single'
+        }];
+    }
 })();
